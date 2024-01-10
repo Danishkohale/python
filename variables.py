0871fc470248fee105f58d0df1e0fdb4d341a739
danish <danikohale2002@gmail.com>
2024-01-10 23:56:04 +0530

    datatype

---
+++ variables.py
@@ -0,0 +1,18 @@
+a = 1
+b = "danish"
+c = 1.1
+
+print("type of DATA is ", type(a))
+print("type of DATA is ", type(b))
+print("type of DATA is ", type(c))
+
+
+list1 = [[8,2.3, [-4,5]], ["apple","banana"]] 
+
+print(list1)
+
+tuple1 = (("parrat","sparrow"),("lion","tiger"))
+print(tuple1)
+
+dir1 = {"name":"sakshi","age":20,"vote":"true"}
+print(dir1)
