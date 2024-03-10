1f6ed8985d3841389e527c70949d3574373a462f
danish <danikohale2002@gmail.com>
2024-03-11 01:03:39 +0530

    python

---
+++ loop.py
@@ -0,0 +1,14 @@
+
+marks = {
+       
+        'hindi': 98,
+        'maths': 65,
+        'Science': 45,
+}
+
+for key, value in marks.items():
+    print(f'subject is {key}')
+    print(f'subject is {value}')
+    
+    
+    
